e7f2ef7242cce2f95b62425306926b2f447d2da2
Rohit1164 <mrrohit1164@gmail.com>
2025-11-06 20:28:19 +0530

    add env variable and some changes

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import connectMongoDB from "./connect.js";
 import useRoute from "./router/route.js";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 8000;
 const app = express();
 
-connectMongoDB("mongodb://localhost:27017/URL_Shortner")
+connectMongoDB(process.env.MONGO_DB_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("MongoDB connection Error", error));
 
